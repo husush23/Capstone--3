4dbd8a96cd8b236703ce12c0fa1407632cf776a7
husush23 <husseinkadare40@gmail.com>
2023-07-03 21:15:33 +0300

    fixed

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 function Currency(props) {
   const {ask, bid, open, low, currency} = props;
-  //   console.log(currency);
+
   return (
     <div className='card'>
       <h3>USD/EUR</h3>
-      <span>top</span>
-      <span>Low</span>
+      <span>{ask}</span>
+      <span>{bid}</span>
     </div>
   );
 }
