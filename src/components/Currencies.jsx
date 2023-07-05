0a3afabb928748c5c6a14f1bda6a5d41f616379e
Hussein Hashi <husseinkadare40@gmail.com>
2023-07-05 16:24:53 +0300

    Revert "Tests "

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -1,41 +1,43 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+/*eslint-disable*/
+import React, {useState} from 'react';
+// import {fetchCrypto} from '../redux/features/moneySlice';
+import {useSelector, useDispatch} from 'react-redux';
 import Currency from './Currency';
-import Navbar from './Navbar';
 import '../styles/currencies.css';
 
 function Currencies() {
-  const currencyList = useSelector((state) => state.crypto.currencies);
+  const dispatch = useDispatch();
+  const currencyList = useSelector(state => state.crypto.currencies);
+  // console.log(currencyList);
   const [searchQuery, setSearchQuery] = useState('');
-  const handleSearch = (e) => setSearchQuery(e.target.value);
+  const handleSearch = e => setSearchQuery(e.target.value);
 
-  const filteredList = currencyList.filter((currency) => currency.ticker.toLowerCase().includes(searchQuery.toLowerCase()));
+  const filteredList = currencyList.filter(currency =>
+    currency.ticker.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <div className="form">
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={handleSearch}
-            placeholder="Search pairs"
-          />
-        </div>
-        <div className="cards">
-          {filteredList.map((currency) => (
-            <Currency
-              currency={currency.ticker}
-              bid={currency.bid}
-              ask={currency.ask}
-              open={currency.open}
-              low={currency.low}
-              key={currency.ticker}
-            />
-          ))}
-        </div>
+    <div className='container'>
+      <div className='form'>
+        <input
+          type='text'
+          value={searchQuery}
+          onChange={handleSearch}
+          placeholder='Search pairs'
+        />
       </div>
-    </>
+      <div className='cards'>
+        {filteredList.map(currency => (
+          <Currency
+            currency={currency.ticker}
+            bid={currency.bid}
+            ask={currency.ask}
+            open={currency.open}
+            low={currency.low}
+            key={currency.ticker}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,40 +1,27 @@
+/*eslint-disable*/
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 function Details() {
   const navigate = useNavigate();
   const location = useLocation();
-  const {
-    ask, bid, open, low, currency,
-  } = location.state;
+  const {ask, bid, open, low, currency} = location.state;
 
   const goBack = () => {
     navigate(-1); // Navigates back to the previous page
   };
 
   return (
-    <div className="popup">
-      <div className="popup-content">
+    <div className='popup'>
+      <div className='popup-content'>
         <h3>{currency}</h3>
-        <p>
-          Ask:&nbsp;
-          {ask}
-        </p>
-        <p>
-          Bid:&nbsp;
-          {bid}
-        </p>
-        <p>
-          Open:&nbsp;
-          {open}
-        </p>
-        <p>
-          Low: &nbsp;
-          {low}
-        </p>
-        <button onClick={goBack} type="submit">
+        <p>Ask:&nbsp; {ask}</p>
+        <p>Bid:&nbsp; {bid}</p>
+        <p>Open:&nbsp; {open}</p>
+        <p>Low: &nbsp;{low}</p>
+        <span className='back' onClick={goBack}>
           &#8592;
-        </button>
+        </span>
       </div>
     </div>
   );
