8ff4d8dea688183229287d7be827b634ecabb391
husush23 <husseinkadare40@gmail.com>
2023-07-05 16:11:12 +0300

    Tests added

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -1,43 +1,41 @@
-/*eslint-disable*/
-import React, {useState} from 'react';
-// import {fetchCrypto} from '../redux/features/moneySlice';
-import {useSelector, useDispatch} from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Currency from './Currency';
+import Navbar from './Navbar';
 import '../styles/currencies.css';
 
 function Currencies() {
-  const dispatch = useDispatch();
-  const currencyList = useSelector(state => state.crypto.currencies);
-  // console.log(currencyList);
+  const currencyList = useSelector((state) => state.crypto.currencies);
   const [searchQuery, setSearchQuery] = useState('');
-  const handleSearch = e => setSearchQuery(e.target.value);
+  const handleSearch = (e) => setSearchQuery(e.target.value);
 
-  const filteredList = currencyList.filter(currency =>
-    currency.ticker.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredList = currencyList.filter((currency) => currency.ticker.toLowerCase().includes(searchQuery.toLowerCase()));
   return (
-    <div className='container'>
-      <div className='form'>
-        <input
-          type='text'
-          value={searchQuery}
-          onChange={handleSearch}
-          placeholder='Search pairs'
-        />
-      </div>
-      <div className='cards'>
-        {filteredList.map(currency => (
-          <Currency
-            currency={currency.ticker}
-            bid={currency.bid}
-            ask={currency.ask}
-            open={currency.open}
-            low={currency.low}
-            key={currency.ticker}
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="form">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearch}
+            placeholder="Search pairs"
           />
-        ))}
+        </div>
+        <div className="cards">
+          {filteredList.map((currency) => (
+            <Currency
+              currency={currency.ticker}
+              bid={currency.bid}
+              ask={currency.ask}
+              open={currency.open}
+              low={currency.low}
+              key={currency.ticker}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
